f4a3153a1fb8ba032730a364664bf21e49a59a8f
jfar5522 <u1329900@utah.edu>
2025-03-03 16:56:31 -0700

    link change

--- python-exercises/week9-1.py
+++ python-exercises/week9-1.py
@@ -1,4 +1,4 @@
-#for this program to work based on the file structure I'm using, check out my github repo!: https://github.com/jfar5522/python-exercises
+#for this program to work based on the file structure I'm using, check out my github repo!: https://github.com/jfar5522/python
 #if you're interested in viewing my previous work check out my old github page!: https://github.com/James1323
 
 file=open(r"..\data\first-file.txt")
