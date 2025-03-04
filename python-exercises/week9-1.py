276fb0b996e067f1392cfef0b583338cfe8d270f
jfar5522 <u1329900@utah.edu>
2025-03-04 01:02:29 -0700

    Minor comment changes.

--- python-exercises/week9-1.py
+++ python-exercises/week9-1.py
@@ -1,4 +1,4 @@
-#for this program to work based on the file structure I'm using, check out my github repo!: https://github.com/jfar5522/python
+#for this program to work based on the file structure I'm personally using, check out my github repo!: https://github.com/jfar5522/python
 #if you're interested in viewing my previous work check out my old github page!: https://github.com/James1323
 
 
