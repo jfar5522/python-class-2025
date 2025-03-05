37fb0bdd56b144c8ba4f0d572ea48f8a0aed92e2
jfar5522 <u1329900@utah.edu>
2025-03-05 11:39:02 -0700

    week 2 programs added!

---
+++ python-exercises/week9-2.py
@@ -0,0 +1,5 @@
+file=open("MyFirstWriteFile.txt", "a")
+file.write("U999888 Fisher")
+file.close()
+
+
---
+++ python-exercises/week9-3.py
@@ -0,0 +1,21 @@
+import game
+
+# file=open("WriteOutput.txt", "w")
+# for line in range(1000):
+#     file.write(f"This is a random number: {game.rollDice()}\n")
+# file.close()
+
+with open("WriteOutput.txt", "w") as file:
+    for line in range(1000):
+        file.write(f"This is a random number: {game.rollDice()}\n")
+
+with open("sentences.txt", "a") as file:
+    while(1):
+        sentence = input("Please enter a sentence to add to the file: ")
+        if sentence in "done, Done":
+            break
+        print(f"You have added {sentence}\n")
+        file.write(f"{sentence}\n")
+
+        
+
